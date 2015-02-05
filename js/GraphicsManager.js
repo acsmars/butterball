72462ccce6221c82a8a4210a41d0851fa4f7c03c
Logan Herche <l_herche@u.pacific.edu>
2015-02-05 14:40:53 -0800

    Added interface of GraphicsManager class.

---
+++ js/GraphicsManager.js
@@ -0,0 +1,20 @@
+var GraphicsManager = function () {
+    var canvas = null;
+
+    var wall_color = null;
+    var ball_color = null;
+
+    // intialize HTML5 canvas.
+    // @param canvas_container Div container for the HTML5 canvas
+    // @param width The width of the canvas
+    // @param height The height of the canvas
+    self.init = function (canvas_container, width, height) {
+
+    };
+
+    // draw objects to the screen
+    // @param objects Array of objects to draw
+    self.draw = function (objects) {
+
+    };
+};
