397b4903bb94602ba4e5759edbf01712f7de7cd6
garyrobertsjr <g_roberts@u.pacific.edu>
2015-02-10 13:59:15 -0800

    Added wall type construct.

--- js/Wall.js
+++ js/Wall.js
@@ -2,6 +2,8 @@
  * Wall class acts as a generic "wall" object.
  */
 function wall (x, y, width, height) {
+	
+	this.type = "wall";
 	this.x = typeof x !== 'undefined' ? x : 0;
 	this.y = typeof y !== 'undefined' ? x : 0;
 	this.width = typeof width !== 'undefined' ? width : 0;
