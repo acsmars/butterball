023c28afaf1489fcdcc9f5147877430349a7e4dd
garyrobertsjr <g_roberts@u.pacific.edu>
2015-02-10 13:56:10 -0800

    Added initialization and default value checking.

--- js/Wall.js
+++ js/Wall.js
@@ -1,10 +1,10 @@
 /*
  * Wall class acts as a generic "wall" object.
  */
-function wall () {
-	this.x = null;
-	this.y = null;
-	this.width = null;
-	this.height = null;
+function wall (x, y, width, height) {
+	this.x = typeof x !== 'undefined' ? x : 0;
+	this.y = typeof y !== 'undefined' ? x : 0;
+	this.width = typeof width !== 'undefined' ? width : 0;
+	this.height = typeof height !== 'undefined' ? height : 0;
 }
 
