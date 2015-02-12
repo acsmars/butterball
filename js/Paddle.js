413c50f6deca2929ce0322c168e0dee82e701daf
Austin <acsmars@gmail.com>
2015-02-11 23:10:12 -0800

    Added Paddle.js and modified index.html to create one paddle object

---
+++ js/Paddle.js
@@ -0,0 +1,14 @@
+/*
+ * Wall class acts as a generic "wall" object.
+ */
+function paddle (x, y, width, height) {
+	
+	this.type = "paddle";
+	this.x = typeof x !== 'undefined' ? x : 0;
+	this.y = typeof y !== 'undefined' ? y : 0;
+	this.width = typeof width !== 'undefined' ? width : 0;
+	this.height = typeof height !== 'undefined' ? height : 0;
+	this.xVelocity = 0;
+	this.yvelocity = 0;
+}
+
