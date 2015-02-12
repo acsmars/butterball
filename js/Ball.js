7728a24ef518c6461c990a96216bd779b2714667
garyrobertsjr <g_roberts@u.pacific.edu>
2015-02-12 12:41:19 -0800

    Added xv and vy velocity variables.

--- js/Ball.js
+++ js/Ball.js
@@ -8,4 +8,7 @@ function ball (color, x, y, r) {
 	this.x = typeof x !== 'undefined' ? x : 0;
 	this.y = typeof y !== 'undefined' ? y : 0;
 	this.r = typeof r !== 'undefined' ? r : 0;
+	this.vx = 0;
+	this.vy = 0;
 }
+
