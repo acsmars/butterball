9781cdd9bf6c6745cccd8e2c2a3153c367f3edd3
unknown <m_schafer1@u.pacific.edu>
2015-02-19 04:09:51 -0800

    index now runs GameManager; the game will be played starting from GameManager from here on out

--- js/GameManager.js
+++ js/GameManager.js
@@ -3,26 +3,27 @@
 	physics (passed through the SimplePhysicsEngine, graphics (using functions from GraphicsManager.js), 
 	and gameplay (score, win/lose, power-ups, etc.)
  */
- 
+	
  // Do we need params for this? Are we creating the objects in the GameManager or just 
  // passing them by reference from index.html to each of the scripts?
  var GameManager = function()
  {
-	// some test objects - these are the same ones that are in index.html
-	var objects = [new wall(300, 300, 50, 50), new ball('green', 200, 200, 10), new paddle(200, 400, 70, 10)];
-	
-	// test physics engine - what are pxWidth, pxHeight, debug in more practical terms?
-	//var phys = new SimplePhysicsEngine(...);
-	
-	// graphics manager from index.html
+	// graphics manager
 	var gfx = new GraphicsManager(document.getElementById("canvas_container"), window.innerHeight, window.innerHeight);
 	
+	// physics engine
+	var phs = new SimplePhysicsEngine(window.innerHeight, window.innerHeight, 0);
+
 	//controller from index.html
 	var con = new Controller(document.getElementById("canvas_container"));
 
+	// some test objects - these are the same ones that are in index.html
+    var objs = [new wall(100, 100, 300, 20), new wall(100, 100, 20, 300), new wall(100, 380, 300, 20), new wall(380, 100, 20, 300), new ball('green', 200, 200, 10), new paddle(200, 400, 70, 10)];
+	
 	// The first player will be player number 0 for code purposes
 	var scores = [0, 0, 0]
 	
+	// Functions to get and set the score externally (possibly unneeded) 
 	function getScore(playerNum)
 	{
 		return scores[playerNum];
@@ -32,7 +33,44 @@
 		scores[playerNum] = val;
 	}
 	
+	gfx.draw(objs, scores);
+	
+	var dir = 2;
+    
+    objs[4].vx = 5;
+    objs[4].vy = 6;
+    objs[0].direction = "h";
+    objs[1].direction = "v";
+    objs[2].direction = "h";
+    objs[3].direction = "v";
+
+    window.setInterval(function() {
+        scores[0] += 1;
+        gfx.draw(objs, scores);
+    }, 1000);
+
+    window.setInterval(function() {
+        scores[1] += 1;
+        gfx.draw(objs, scores);
+    }, 2400);
+
+    window.setInterval(function() {
+        scores[2] += 1;
+        gfx.draw(objs, scores);
+    }, 23421);
+
+    window.setInterval(function() {
+        phs.step(objs, 1);
+        gfx.draw(objs, scores);
+    }, 10);
+
+    window.setInterval(function() {
+        dir = -dir;
+    }, 1250);
+	
 	// Continually pass data to physics engine and graphics manager
+	// TODO: implement time-step instead of (1)
+	/*
 	while(1)
 	{
 		// Send data to the physics engine (pass by reference)
@@ -42,5 +80,6 @@
 		gfx.draw(objs, scores);
 		// TODO: needs testing and input by team members
 	}
+	*/
 }
 	
