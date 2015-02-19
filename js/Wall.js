30e7de076c5798c4956be509173075bbc03117e5
Warren Whitman <w_whitman@u.pacific.edu>
2015-02-18 17:48:54 -0800

    Initial version of edge collisions done, repositioning bugged in this commit.

--- js/Wall.js
+++ js/Wall.js
@@ -1,13 +1,14 @@
 /*
  * Wall class acts as a generic "wall" object.
  */
-function wall (x, y, width, height) {
+function wall (x, y, width, height, direction) {
 	
 	this.type = "wall";
 	this.x = typeof x !== 'undefined' ? x : 0;
 	this.y = typeof y !== 'undefined' ? y : 0;
 	this.width = typeof width !== 'undefined' ? width : 0;
 	this.height = typeof height !== 'undefined' ? height : 0;
-	this.direction = "v";
+	this.owner = typeof owner !== 'undefined' ? owner : null;
+	this.direction = typeof direction !== 'undefined' ? direction : "v";
 }
 
