6fd701aa59845f59e2d8ce609a61f7c9786b90a3
garyrobertsjr <g_roberts@u.pacific.edu>
2015-03-23 08:39:33 -0700

    Changed pushState func to non-private membership.

--- client/js/NetworkManager.js
+++ client/js/NetworkManager.js
@@ -4,7 +4,7 @@ var socket = io();
  * Pushes the statePackage via socket to server as a JSON.
  * @Param {string} Stringified JSON state package to be passed to server
  */
-var pushState = function (statePackage) {
+function pushState (statePackage) {
 	socket.emit('tickEvent', statePackage);
 	});
 });
