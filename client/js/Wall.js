ca9a0142e4c7891471b9dce0b17b01be80e8ded6
Austin <acsmars@gmail.com>
2015-03-23 16:50:21 -0700

    Added color parameter for wall. Uses default brown if given empty string or 'default'

--- client/js/Wall.js
+++ client/js/Wall.js
@@ -1,15 +1,15 @@
 /*
  * Wall class acts as a generic "wall" object.
  */
-function wall (x, y, width, height,value) {
+function wall (color, x, y, width, height,value) {
 	
 	this.type = "wall";
+	this.color = (color !== '' && color !== "default") ? color: "#8C6542";
 	this.x = typeof x !== 'undefined' ? x : 0;
 	this.y = typeof y !== 'undefined' ? y : 0;
 	this.width = typeof width !== 'undefined' ? width : 0;
 	this.height = typeof height !== 'undefined' ? height : 0;
 	this.owner = typeof owner !== 'undefined' ? owner : null;
 	this.value = typeof value !== 'undefined' ? value : 0;
-	//this.direction = typeof direction !== 'undefined' ? direction : "v";
 }
 
