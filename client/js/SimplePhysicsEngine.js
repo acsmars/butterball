89c99bf9d6b8adc1cfafde030631783ed9259c83
Warren Whitman <w_whitman@u.pacific.edu>
2015-03-23 02:00:21 -0700

    Added framework code to SimplePhysicsEngine to allow sending paddle state network events on tick.

--- client/js/SimplePhysicsEngine.js
+++ client/js/SimplePhysicsEngine.js
@@ -424,6 +424,16 @@ var SimplePhysicsEngine = function (physWidth, physHeight, maxSpeed, debug) {
                 newObjects[index].y = physHeight / 2;
             }
         }
+        
+        // Finally, send JSON-encoded paddle object for this client's paddle
+        //TODO: Function in tickService.js is currently private, or I don't know how to reference it correctly.
+        //TODO: How are teams assigned on a per-client basis?
+        var myTeam = 0 // For testing purposes
+        for (i = 0; i < team.length; i++) {
+          if (objects[index2].hasOwnProperty("owner") && objects[index2].owner !== null && objects[index2].owner == myTeam) {
+              pushState(JSON.stringify(objects[index2]));
+          }
+        }
     };
 
     init(physWidth, physHeight, debug);
