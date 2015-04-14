0a5a6639b20bd91470d25b2d6bab043a10fe4c76
Warren Whitman <w_whitman@u.pacific.edu>
2015-04-14 15:28:25 -0700

    Fixed not sending UUID as part of network packet for server organization purposes.

--- client/js/SimplePhysicsEngine.js
+++ client/js/SimplePhysicsEngine.js
@@ -434,7 +434,7 @@ var SimplePhysicsEngine = function (physWidth, physHeight, maxSpeed, debug) {
                     pushState(JSON.stringify(objects[index]));
                 }
             }*/
-            pushState(JSON.stringify(objects[UUID]));
+            pushState(JSON.stringify([UUID, objects[UUID]]));
         }
 
     };
