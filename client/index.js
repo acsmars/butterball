b3d402ea7f460da9ea4e07993f4dc92fa2cc7c32
garyrobertsjr <g_roberts@u.pacific.edu>
2015-04-15 18:04:27 -0700

    State caching implemented in NetwrkMgr.

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var stateCache = []
 //Serve HTTP requests and append console log.
 app.get('/game.html' ,function(req, res){
 	var id = req.param('id');
@@ -19,13 +20,28 @@ io.on('connection', function(socket) {
 	//Report TCP connection in console log.
 	console.log('TCP Connection Established:', socket.request.connection._peername);
 	
-	//Report debug msg in console log.
+	//Store received tickEvents
 	socket.on('tickEvent', function(tickPkg){
-	    console.log(tickPkg);
-	    io.emit('statePush', tickPkg);
-	    console.log('Push');
+	    //console.log(tickPkg);
+	    //io.emit('statePush', tickPkg);
+	    //console.log('Push');
+	    var paddle = JSON.parse(tickPkg)[1]
+	    var id = JSON.parse(tickPkg)[0]
+
+	    if(id == 4){
+	    	stateCache[0] = paddle;
+	    }
+	    if(id == 5){
+	    	stateCache[1] = paddle;
+	    }
 	    });
+
+	setInterval(function () {
+		io.emit('statePush', JSON.stringify(stateCache));
+		console.log(stateCache);		
+		}, 100);
 });
+
 
 //Listen for connections and request on port 8080
 http.listen(8080, function(){
